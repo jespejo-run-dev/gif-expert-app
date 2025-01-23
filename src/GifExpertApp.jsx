a7777ddb27e9f9716c5885a522d50df84531ef4c
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-23 11:43:30 -0300

    GifGrid - Nuevo componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
@@ -20,22 +21,21 @@ export const GifExpertApp = () => {
 
     return (
         <>
-            {/* Titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
             <AddCategory 
-                // setCategories={setCategories} 
                 onNewCategory={value => onAddCategory(value)}
             />
 
-            {/* Listado de Gif */}
-            <ol>
-                {categories.map( (category, i) => {
-                    return <li key={i}>{category}</li>
-                })}
-            </ol>
-                {/* Gif Item */}
+            {
+                categories.map( (category) => (
+                        <GifGrid 
+                            key={category} 
+                            category={category}
+                        />
+                    )
+                )
+            }
         </>
     )
 }
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,12 @@
+
+export const GifGrid = ({category}) => {
+
+    // API
+
+    return (
+        <>
+            <h3>{category}</h3> 
+
+        </>
+    )
+}
