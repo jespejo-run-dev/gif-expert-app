3d9219857c48f99c2c8708e54c8f1e8d54323412
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-27 15:30:27 -0300

    Despliegue con variables de entorno

--- src/helpers/getGifts.js
+++ src/helpers/getGifts.js
@@ -1,18 +1,17 @@
-export const getGifs = async(category) =>{
-
-    const apiKey = 'gpl09YDu5saQWihh0VcqitpXfjMfCReG';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
+export const getGifs = async (category) => {
+    // Usar la clave de API desde la variable de entorno
+    const apiKey = import.meta.env.VITE_GIPHY_API_KEY;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
 
     const resp = await fetch(url);
-    const {data} = await resp.json();
+    const { data } = await resp.json();
 
     const gifs = data.map(img => ({
         id: img.id,
         title: img.title,
-        url: img.images.downsized_medium.url
-    }))
+        url: img.images.downsized_medium.url,
+    }));
 
     console.log(gifs);
     return gifs;
-    
-}
+};
