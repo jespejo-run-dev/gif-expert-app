f513e6f1b5ef7bf2391696585408e020d05caff9
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-01-27 14:06:18 -0300

    Mostrar mensaje de carga

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,6 +9,11 @@ export const GifGrid = ({category}) => {
     return (
         <>
             <h3>{category}</h3> 
+
+            {
+                isLoading && <p>Cargando...</p>
+            }
+
             <div className="card-grid">
                 {
                     images.map((image) => (
